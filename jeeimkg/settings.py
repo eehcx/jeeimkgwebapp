3a8eac9671641a8de4f9a975893ffe1ff36906da
Enoc Emanuel <eehcx.contacto@gmail.com>
2023-03-22 16:28:09 -0600

    elimacion de archivos

--- jeeimkg/settings.py
+++ jeeimkg/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-18$e+qc0y4smz)lk$k)o^h9dtkj7f2fvkrst(=!3luvlqrql4+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
